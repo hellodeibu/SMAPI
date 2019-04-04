0fdc8ae8b4af53dd0f911e690818d30c64e6f16f
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-04-04 18:43:56 -0400

    bump version to 3.0

--- build/GlobalAssemblyInfo.cs
+++ build/GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("SMAPI")]
-[assembly: AssemblyVersion("2.11.2")]
-[assembly: AssemblyFileVersion("2.11.2")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
